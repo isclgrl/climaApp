91d62a2eff4778f20e0787999e2613d33e2243ed
Luis Gabriel Rodriguez <35466463+isclgrl@users.noreply.github.com>
2022-03-09 14:30:10 -0600

    add options in menu

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const { inquireMenu,
-        leerInput,
-        pausa
-    } = require('./helpers/inquirer');
+    leerInput,
+    pausa
+} = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
-const main = async() => {
+const main = async () => {
+
     console.clear();
 
+    const busquedas = new Busquedas();
     let opt;
 
-    do{
-        opt = await inquireMenu(); 
-        console.log({opt})
+    do {
+        opt = await inquireMenu();
 
-
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+                // Buscar lugares
+                // Seleccionar lugar
+                // Mostrar resultados
+                console.log('\ninformacion del lugar\n'.green);
+                console.log('Ciudad: ',);
+                console.log('Lat: ');
+                console.log('Lng: ');
+                console.log('Temperatura: ');
+                console.log('Minima: ');
+                console.log('Maxima: ');
+            break;
+        }
 
         if (opt !== 0) await pausa();
 
-        // switch(opt){
-
-        // }
-
-    } while(opt !== 0);
+    } while (opt !== 0);
 
 }
 
