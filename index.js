bcf703eae13f69ef3eac42d71fb08aa201c08838
Luis Gabriel Rodriguez <35466463+isclgrl@users.noreply.github.com>
2022-03-09 14:00:52 -0600

    show menu inquirer

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const {leerInput} = require('./helpers/inquirer');
+const { inquireMenu,
+        leerInput,
+        pausa
+    } = require('./helpers/inquirer');
 
 const main = async() => {
+    console.clear();
 
-    const texto = await leerInput('Hola: ');
+    let opt;
 
-    console.log(texto);
+    do{
+        opt = await inquireMenu(); 
+        console.log({opt})
+
+
+
+        if (opt !== 0) await pausa();
+
+        // switch(opt){
+
+        // }
+
+    } while(opt !== 0);
 
 }
 
