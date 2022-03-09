70f9f1187ae3582746234419237254c7d3f844a9
Luis Gabriel Rodriguez <35466463+isclgrl@users.noreply.github.com>
2022-03-09 14:29:52 -0600

    create class file

---
+++ models/busquedas.js
@@ -0,0 +1,18 @@
+
+class Busquedas {
+
+    historial = ['Madrid', 'Tokio'];
+
+    constructor(){
+        //TODO: leer db si existe
+    }
+
+    async ciudad(lugar = '') {
+        //peticion http
+        console.log(lugar);
+
+        return []; // retornar los lugares 
+    }
+}
+
+module.exports = Busquedas;
